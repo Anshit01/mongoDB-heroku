8ecfeb0f53128d7fbab1e17b89e246a4e0577d70
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-05-05 13:02:04 -0500

    load_all route added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from pymongo import MongoClient
+import requests
 
 app = Flask(__name__)
 
+cluster = MongoClient('mongodb+srv://anshit01:uu66xdT!A4aQL-e@mongodb-heroku-db-uw5of.mongodb.net/test?retryWrites=true&w=majority')
+collection = cluster['test-db']['test-collection']
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/load_all')
+def load_all():
+    results = collection.find({})
+    s = ''
+    for result in results:
+        s += str(result) + '<br>'
+    return s
