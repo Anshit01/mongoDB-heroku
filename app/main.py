14791945c8c75ac0ea4fb386e0b015b2f088705c
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-05-05 13:53:15 -0500

    fixed

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
-#from pymongo import MongoClient
+from pymongo import MongoClient
 import requests
 
 app = Flask(__name__)
 
-#cluster = MongoClient('mongodb+srv://anshit01:uu66xdT!A4aQL-e@mongodb-heroku-db-uw5of.mongodb.net/test?retryWrites=true&w=majority')
-#collection = cluster['test-db']['test-collection']
+cluster = MongoClient('mongodb+srv://anshit01:uu66xdT!A4aQL-e@mongodb-heroku-db-uw5of.mongodb.net/test?retryWrites=true&w=majority')
+collection = cluster['test-db']['test-collection']
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/load_all')
-# def load_all():
-#     results = collection.find({})
-#     s = ''
-#     for result in results:
-#         s += str(result) + '<br>'
-#     return s
+@app.route('/load_all')
+def load_all():
+    results = collection.find({})
+    s = ''
+    for result in results:
+        s += str(result) + '<br>'
+    return s
 
 @app.route('/request')
 def request():
